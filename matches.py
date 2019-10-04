6c7597d027e007155e28dd5a99c125064d5132ec
Lucas <lfeashby@gmail.com>
2019-10-03 22:14:28 -0400

    Revised path extension

--- matches.py
+++ matches.py
@@ -1,304 +1,304 @@
-# TODO: Check path after pulling down tsv_files wikipron branch, add unpacked tar folder names
+# TODO: Check path after pulling down tsv wikipron branch, add unpacked tar folder names
 
 MATCHES = {
   #Albanian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/alb_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/alb_phonemic.tsv",
     "file_name": "merged_tsvs/alb_phonemic_frequencies.tsv"
   },
   # Arabic
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/ara_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/ara_phonemic.tsv",
     "file_name": "merged_tsvs/ara_phonemic_frequencies.tsv"
   },
   # Armenian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/arm_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/arm_phonetic.tsv",
     "file_name": "merged_tsvs/arm_phonetic_frequencies.tsv"
   },
   # Assamese
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/asm_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/asm_phonemic.tsv",
     "file_name": "merged_tsvs/asm_phonemic_frequencies.tsv"
   },
   # Azerbaijani
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/aze_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/aze_phonetic.tsv",
     "file_name": "merged_tsvs/aze_phonetic_frequencies.tsv"
   },
   # Bashkir
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/bak_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/bak_phonetic.tsv",
     "file_name": "merged_tsvs/bak_phonetic_frequencies.tsv"
   },
   # Belarusian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/bel_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/bel_phonetic.tsv",
     "file_name": "merged_tsvs/bel_phonetic_frequencies.tsv"
   },
   # Bulgarian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/bul_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/bul_phonemic.tsv",
     "file_name": "merged_tsvs/bul_phonemic_frequencies.tsv"
   },
   # Catalan
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/cat_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/cat_phonemic.tsv",
     "file_name": "merged_tsvs/cat_phonemic_frequencies.tsv"
   },
   # Czech
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/cze_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/cze_phonetic.tsv",
     "file_name": "merged_tsvs/cze_phonetic_frequencies.tsv"
   },
   # Danish
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/dan_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/dan_phonetic.tsv",
     "file_name": "merged_tsvs/dan_phonetic_frequencies.tsv"
   },
   # Dutch
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/dut_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/dut_phonemic.tsv",
     "file_name": "merged_tsvs/dut_phonemic_frequencies.tsv"
   },
   # English
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/eng_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/eng_phonemic.tsv",
     "file_name": "merged_tsvs/eng_phonemic_frequencies.tsv"
   },
   # Esperanto
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/epo_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/epo_phonemic.tsv",
     "file_name": "merged_tsvs/epo_phonemic_frequencies.tsv"
   },
   # Faroese
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/fao_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/fao_phonemic.tsv",
     "file_name": "merged_tsvs/fao_phonemic_frequencies.tsv"
   },
   # Finnish
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/fin_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/fin_phonemic.tsv",
     "file_name": "merged_tsvs/fin_phonemic_frequencies.tsv"
   },
   # French
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/fre_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/fre_phonemic.tsv",
     "file_name": "merged_tsvs/fre_phonemic_frequencies.tsv"
   },
   # Galician
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/glg_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/glg_phonemic.tsv",
     "file_name": "merged_tsvs/glg_phonemic_frequencies.tsv"
   },
   # Georgian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/geo_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/geo_phonemic.tsv",
     "file_name": "merged_tsvs/geo_phonemic_frequencies.tsv"
   },
   # German
   "" : {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/ger_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/ger_phonemic.tsv",
     "file_name": "merged_tsvs/ger_phonemic_frequencies.tsv"
   },
   # Greek
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/gre_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/gre_phonemic.tsv",
     "file_name": "merged_tsvs/ger_phonemic_frequencies.tsv"
   },
   # Hindi
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/hin_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/hin_phonemic.tsv",
     "file_name": "merged_tsvs/hin_phonemic_frequencies.tsv"
   },
   # Hungarian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/hun_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/hun_phonetic.tsv",
     "file_name": "merged_tsvs/hun_phonetic_frequencies.tsv"
   },
   # Icelandic
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/ice_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/ice_phonemic.tsv",
     "file_name": "merged_tsvs/ice_phonemic_frequencies.tsv"
   },
   # Ido
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/ido_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/ido_phonemic.tsv",
     "file_name": "merged_tsvs/ido_phonemic_frequencies.tsv"
   },
   # Indonesian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/ind_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/ind_phonemic.tsv",
     "file_name": "merged_tsvs/ind_phonemic_frequencies.tsv"
   },
   # Irish
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/gle_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/gle_phonemic.tsv",
     "file_name": "merged_tsvs/gle_phonemic_frequencies.tsv"
   },
   # Italian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/ita_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/ita_phonemic.tsv",
     "file_name": "merged_tsvs/ita_phonemic_frequencies.tsv"
   },
   # Japanese
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/jpn_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/jpn_phonetic.tsv",
     "file_name": "merged_tsvs/jpn_phonetic_frequencies.tsv"
   },
   # Korean
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/kor_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/kor_phonetic.tsv",
     "file_name": "merged_tsvs/kor_phonetic_frequencies.tsv"
   },
   # Kurdish
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/kur_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/kur_phonemic.tsv",
     "file_name": "merged_tsvs/kur_phonemic_frequencies.tsv"
   },
   # Latin
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/lat_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/lat_phonemic.tsv",
     "file_name": "merged_tsvs/lat_phonemic_frequencies.tsv"
   },
   # Latvian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/lav_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/lav_phonetic.tsv",
     "file_name": "merged_tsvs/lav_phonetic_frequencies.tsv"
   },
   # Lithuanian | Lithuanian | True | Phonetic | 12730 |
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/lit_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/lit_phonetic.tsv",
     "file_name": "merged_tsvs/lit_phonetic_frequencies.tsv"
   },
   # Lower Sorbian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/dsb_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/dsb_phonemic.tsv",
     "file_name": "merged_tsvs/dsb_phonemic_frequencies.tsv"
   },
   # Luxembourgish
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/ltz_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/ltz_phonemic.tsv",
     "file_name": "merged_tsvs/ltz_phonemic_frequencies.tsv"
   },
   # Macedonian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/mac_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/mac_phonetic.tsv",
     "file_name": "merged_tsvs/mac_phonetic_frequencies.tsv"
   },
   # Malay
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/may_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/may_phonemic.tsv",
     "file_name": "merged_tsvs/may_phonemic_frequencies.tsv"
   },
   # Maltese
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/mlt_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/mlt_phonemic.tsv",
     "file_name": "merged_tsvs/mlt_phonemic_frequencies.tsv"
   },
   # Mongoloian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/mon_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/mon_phonemic.tsv",
     "file_name": "merged_tsvs/mon_phonemic_frequencies.tsv"
   },
   # Northern Sami
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/sme_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/sme_phonemic.tsv",
     "file_name": "merged_tsvs/sme_phonemic_frequencies.tsv"
   },
   # Norwegian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/nor_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/nor_phonemic.tsv",
     "file_name": "merged_tsvs/nor_phonemic_frequencies.tsv"
   },
   # Norwegian Bokmål
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/nob_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/nob_phonemic.tsv",
     "file_name": "merged_tsvs/nob_phonemic_frequencies.tsv"
   },
   # Norwegian Nynorsk
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/nno_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/nno_phonemic.tsv",
     "file_name": "merged_tsvs/nno_phonemic_frequencies.tsv"
   },
   # Persian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/per_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/per_phonemic.tsv",
     "file_name": "merged_tsvs/per_phonemic_frequencies.tsv"
   },
   # Polish
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/pol_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/pol_phonemic.tsv",
     "file_name": "merged_tsvs/pol_phonemic_frequencies.tsv"
   },
   # Portuguese
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/por_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/por_phonemic.tsv",
     "file_name": "merged_tsvs/por_phonemic_frequencies.tsv"
   },
   # Romanian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/rum_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/rum_phonetic.tsv",
     "file_name": "merged_tsvs/rum_phonetic_frequencies.tsv"
   },
   # Sanskrit
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/san_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/san_phonemic.tsv",
     "file_name": "merged_tsvs/san_phonemic_frequencies.tsv"
   },
   # Slovak
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/slo_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/slo_phonemic.tsv",
     "file_name": "merged_tsvs/slo_phonemic_frequencies.tsv"
   },
   # Slovenian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/slv_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/slv_phonemic.tsv",
     "file_name": "merged_tsvs/slv_phonemic_frequencies.tsv"
   },
   # Spanish
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/spa_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/spa_phonemic.tsv",
     "file_name": "merged_tsvs/spa_phonemic_frequencies.tsv"
   },
   # Swedish
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/swe_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/swe_phonemic.tsv",
     "file_name": "merged_tsvs/swe_phonemic_frequencies.tsv"
   },
   # Tagalog
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/tgl_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/tgl_phonemic.tsv",
     "file_name": "merged_tsvs/tgl_phonemic_frequencies.tsv"
   },
   # Tamil
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/tam_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/tam_phonemic.tsv",
     "file_name": "merged_tsvs/tam_phonemic_frequencies.tsv"
   },
   # Turkish
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/tur_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/tur_phonetic.tsv",
     "file_name": "merged_tsvs/tur_phonetic_frequencies.tsv"
   },
   # Ukrainian
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/ukr_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/ukr_phonetic.tsv",
     "file_name": "merged_tsvs/ukr_phonetic_frequencies.tsv"
   },
   # Vietnamese
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/vie_phonetic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/vie_phonetic.tsv",
     "file_name": "merged_tsvs/vie_phonetic_frequencies.tsv"
   },
   # Welsh
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/wel_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/wel_phonemic.tsv",
     "file_name": "merged_tsvs/wel_phonemic_frequencies.tsv"
   },
   # Zulu
   "": {
-    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv_files/zul_phonemic.tsv",
+    "path": "/home/compling7/Desktop/tsv_testing/wikipron/languages/wikipron/tsv/zul_phonemic.tsv",
     "file_name": "merged_tsvs/zul_phonemic_frequencies.tsv"
   }
 }
